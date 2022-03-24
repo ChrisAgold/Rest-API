06326e579c5f23a5788f56b433e5571d16c42917
Chris Agold <christophersagold@gmail.com>
2022-03-24 17:53:30 -0500

    added post request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,11 +1,15 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+// middleware
+app.use(express.json());
+
 // GET request example data
 const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
 ];
 
 // Define Route/Path => Route Handler
@@ -25,6 +29,50 @@ app.get('/api/courses/:id', (req, res) => {
     if (!course) res.status(404).send('The course with the given ID was not found!');
     res.send(course);
 });
+
+// POST new course to courses array
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body); // result.error
+    if (error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    // push object to array
+    courses.push(course);
+    res.send(course);
+});
+
+// PUT
+app.put('/api/courses/:id', (req,res) => {
+    // Look up the course
+    // If not existing, return 404
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('The course with the given ID was not found!');
+    // Validate
+    // If invalid, return 400 - Bad request
+
+    const { error } = validateCourse(req.body); // result.error
+    if (error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    // Update course
+    course.name = req.body.name;
+    // Return the updated course
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+    };
+    return Joi.validate(course, schema);
+}
 
 // Will run on port or localhost 3000
 const port = process.env.PORT || 3000;
